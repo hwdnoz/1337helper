76755b0faeb2002630e216d49c3a83d1f201c2c6
Howard Noz <hwdnoz@users.noreply.github.com>
2025-12-31 02:17:45 -0800

    Add docker config for node backend

--- backend-node/app.js
+++ backend-node/app.js
@@ -11,7 +11,7 @@ import cacheRoutes from './routes/cacheRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3102;
 
 // Middleware
 app.use(cors());
